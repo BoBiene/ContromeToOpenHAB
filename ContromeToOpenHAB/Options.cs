98117c0fe8cfcb84956c658570dd17a642f13b62
Bo Biene <biene@inray.de>
2017-09-18 20:03:17 +0200

    Documentation

--- ContromeToOpenHAB/Options.cs
+++ ContromeToOpenHAB/Options.cs
@@ -10,20 +10,23 @@ namespace ContromeToOpenHAB
 {
     internal class Options
     {
-        [Option('a', "addr", Required = true, HelpText = "The IP-Address oder DNS name of the Controm Mini-Server (e.g 192.168.1.100 or contromeServer)")]
+        [Option('a', "addr", Required = true, HelpText = "The IP-Address oder DNS name of the Controme-Mini-Server (e.g 192.168.1.100 or contromeServer)")]
         public string ContromURL { get; set; }
 
-        [Option('u', "user",Required =true , HelpText = "The UserName openHAB wil use to set Values")]
+        [Option('u', "user",Required =true , HelpText = "The UserName openHAB will use to set Values")]
         public string Username { get; set; }
 
-        [Option('p', "password", Required =true, HelpText = "The Password for the User (Hint: the password is stored in plain text in the Config-File)")]
+        [Option('p', "password", Required =true, HelpText = "The Password for the User (Hint: the password is stored in plain text in the config-File)")]
         public string Password { get; set; }
 
-        [Option('h', "houseid", Required = false,DefaultValue =1,  HelpText = "The House-ID in the controme Server to use, default is 1")]
+        [Option('h', "houseid", Required = false,DefaultValue =1,  HelpText = "The House-ID in the Controme Server to use, default is 1")]
         public int HouseID { get; set; }
 
         [Option('o', "output", Required = false, DefaultValue = "", HelpText = "Target directory to create the openHAB files in.")]
         public string OutputDir { get; set; }
+
+        [Option('c',"cacheUrl",Required =false,DefaultValue ="controme",HelpText ="The HTTP-Cache-Entry to point to the Controme-Mini-Server. Set to empty to disable.")]
+        public string ContromCacheURL { get; set; }
 
         [HelpOption]
         public string GetUsage()
--- ContromeToOpenHAB/Program.cs
+++ ContromeToOpenHAB/Program.cs
@@ -19,74 +19,83 @@ namespace ContromeToOpenHAB
             {
                 Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
             }
-
-            using (StreamWriter rulesFile = CreateRules(options))
+            try
             {
-                using (StreamWriter itemsFile = CreateConfigFile(options, "items\\controme.items"))
+                using (StreamWriter rulesFile = CreateRules(options))
                 {
-                    using (StreamWriter sitemapFile = CreateSitemap(options))
+                    using (StreamWriter itemsFile = CreateConfigFile(options, "items\\controme.items"))
                     {
-
-
-                        Uri contromeBaseUrl = new Uri($"http://{options.ContromURL}/");
-
-                        RestRequest request = new RestRequest("get/json/v1/{HouseID}/{Method}");
-                        request.AddUrlSegment("HouseID", options.HouseID.ToString());
-                        request.AddUrlSegment("Method", "temps");
-
-                        RestClient client = new RestClient(contromeBaseUrl);
-
-                        var response = client.Execute(request);
-
-                        JArray json = JArray.Parse(response.Content);
-
-                        foreach (JObject floor in json)
+                        using (StreamWriter sitemapFile = CreateSitemap(options))
                         {
-                            string strFloorName = floor["etagenname"].Value<string>();
 
-                            Console.WriteLine("Creating files for floor " + strFloorName);
 
-                            sitemapFile.WriteLine($"Frame label=\"{strFloorName}\" {{");
-                            string strFloorPrefix = new string(Escape(strFloorName).ToArray()) + "_";
-                            JArray rooms = floor["raeume"].Value<JArray>();
+                            Uri contromeBaseUrl = new Uri($"http://{options.ContromURL}/");
 
-                            foreach (var room in rooms)
+                            RestRequest request = new RestRequest("get/json/v1/{HouseID}/{Method}");
+                            request.AddUrlSegment("HouseID", options.HouseID.ToString());
+                            request.AddUrlSegment("Method", "temps");
+
+                            RestClient client = new RestClient(contromeBaseUrl);
+                            string strCacheUrl = options.ContromCacheURL;
+                            if (string.IsNullOrEmpty(strCacheUrl))
+                                strCacheUrl = $"http://{options.ContromURL}/get/json/v1/{options.HouseID}/temps/";
+
+                            var response = client.Execute(request);
+
+                            JArray json = JArray.Parse(response.Content);
+
+                            foreach (JObject floor in json)
                             {
-                                string strRoomName = room["name"].Value<string>();
-                                string strRoomID = room["id"].Value<string>();
-                                string strEscapedName = strFloorPrefix + new string(Escape(strRoomName).ToArray());
+                                string strFloorName = floor["etagenname"].Value<string>();
 
-                                Console.WriteLine("Creating entries for  " + strRoomName);
+                                Console.WriteLine("Creating files for floor " + strFloorName);
 
-                                itemsFile.WriteLine($"String Controme_Raw_{strEscapedName} {{http = \"<[controme:10000:JSONPATH($..raeume[?(@.id=={strRoomID})].temperatur)]\"}}");
-                                itemsFile.WriteLine($"String Controme_Raw_{strEscapedName}_Soll {{http = \"<[controme:10000:JSONPATH($..raeume[?(@.id=={strRoomID})].solltemperatur)]\"}}");
+                                sitemapFile.WriteLine($"Frame label=\"{strFloorName}\" {{");
+                                string strFloorPrefix = new string(Escape(strFloorName).ToArray()) + "_";
+                                JArray rooms = floor["raeume"].Value<JArray>();
 
-                                itemsFile.WriteLine($"roup g{strEscapedName}Thermostat \"{strRoomName}\" (gFF) [ \"Thermostat\" ]");
-                                itemsFile.WriteLine($"Number Controme_Proxy_{strEscapedName} \"{strRoomName} [% .2f °C]\"  (g{strEscapedName}Thermostat) [ \"CurrentTemperature\" ]");
-                                itemsFile.WriteLine($"Number Controme_Proxy_{strEscapedName}_Soll \"{strRoomName} Soll[% .1f °C]\"  (g{strEscapedName}Thermostat) [ \"TargetTemperature\" ]");
-                                itemsFile.WriteLine();
+                                foreach (var room in rooms)
+                                {
+                                    string strRoomName = room["name"].Value<string>();
+                                    string strRoomID = room["id"].Value<string>();
+                                    string strEscapedName = strFloorPrefix + new string(Escape(strRoomName).ToArray());
+
+                                    Console.WriteLine("Creating entries for  " + strRoomName);
+
+                                    itemsFile.WriteLine($"String Controme_Raw_{strEscapedName} {{http = \"<[{strCacheUrl}:10000:JSONPATH($..raeume[?(@.id=={strRoomID})].temperatur)]\"}}");
+                                    itemsFile.WriteLine($"String Controme_Raw_{strEscapedName}_Soll {{http = \"<[{strCacheUrl}:10000:JSONPATH($..raeume[?(@.id=={strRoomID})].solltemperatur)]\"}}");
+
+                                    itemsFile.WriteLine($"Group g{strEscapedName}Thermostat \"{strRoomName}\" (gFF) [ \"Thermostat\" ]");
+                                    itemsFile.WriteLine($"Number Controme_Proxy_{strEscapedName} \"{strRoomName} [% .2f °C]\"  (g{strEscapedName}Thermostat) [ \"CurrentTemperature\" ]");
+                                    itemsFile.WriteLine($"Number Controme_Proxy_{strEscapedName}_Soll \"{strRoomName} Soll[% .1f °C]\"  (g{strEscapedName}Thermostat) [ \"TargetTemperature\" ]");
+                                    itemsFile.WriteLine();
 
 
 
-                                rulesFile.WriteLine($"rule \"Unpack JSON Value {strEscapedName}\" when System started or Item Controme_Raw_{strEscapedName} changed then ContromeUnpackJsonArray.apply(Controme_Proxy_{strEscapedName}, Controme_Raw_{strEscapedName}) end");
-                                rulesFile.WriteLine($"rule \"Unpack JSON Value {strEscapedName}_Soll\" when System started or Item Controme_Raw_{strEscapedName}_Soll changed then ContromeUnpackJsonArray.apply(Controme_Proxy_{strEscapedName}_Soll, Controme_Raw_{strEscapedName}_Soll) end");
-                                rulesFile.WriteLine($"rule \"Delegate Set - {strEscapedName}\" when Item Controme_Proxy_{strEscapedName}_Soll received command then executeCommandLine(\"curl -X POST -F user={options.Username} -F password={options.Password} -F soll=\"+receivedCommand.toString+\" http://{options.ContromURL}/set/json/v1/{options.HouseID}/soll/{strRoomID}/\") end ");
-                                rulesFile.WriteLine();
+                                    rulesFile.WriteLine($"rule \"Unpack JSON Value {strEscapedName}\" when System started or Item Controme_Raw_{strEscapedName} changed then ContromeUnpackJsonArray.apply(Controme_Proxy_{strEscapedName}, Controme_Raw_{strEscapedName}) end");
+                                    rulesFile.WriteLine($"rule \"Unpack JSON Value {strEscapedName}_Soll\" when System started or Item Controme_Raw_{strEscapedName}_Soll changed then ContromeUnpackJsonArray.apply(Controme_Proxy_{strEscapedName}_Soll, Controme_Raw_{strEscapedName}_Soll) end");
+                                    rulesFile.WriteLine($"rule \"Delegate Set - {strEscapedName}\" when Item Controme_Proxy_{strEscapedName}_Soll received command then executeCommandLine(\"curl -X POST -F user={options.Username} -F password={options.Password} -F soll=\"+receivedCommand.toString+\" http://{options.ContromURL}/set/json/v1/{options.HouseID}/soll/{strRoomID}/\") end ");
+                                    rulesFile.WriteLine();
 
 
-                                sitemapFile.WriteLine($"Text item=Controme_Proxy_{strEscapedName} icon=\"TemperaturSensor\"");
-                                sitemapFile.WriteLine($"Setpoint item=Controme_Proxy_{strEscapedName}_Soll step=0.5 minValue=15 maxValue=26 icon=\"TemperaturSensor\"");
+                                    sitemapFile.WriteLine($"Text item=Controme_Proxy_{strEscapedName} icon=\"TemperaturSensor\"");
+                                    sitemapFile.WriteLine($"Setpoint item=Controme_Proxy_{strEscapedName}_Soll step=0.5 minValue=15 maxValue=26 icon=\"TemperaturSensor\"");
+                                }
+
+                                sitemapFile.WriteLine("}");
                             }
 
                             sitemapFile.WriteLine("}");
                         }
-
-                        sitemapFile.WriteLine("}");
                     }
                 }
+                DirectoryInfo confDir = new DirectoryInfo(Path.Combine(options.OutputDir, "conf"));
+                Console.WriteLine("Created config files at " + confDir.FullName);
             }
-
-            Console.ReadLine();
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed: " + ex.ToString());
+            }
         }
 
    
